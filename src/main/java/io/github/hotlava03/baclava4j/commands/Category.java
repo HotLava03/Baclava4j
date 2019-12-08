f5bba1f64af9b025fb0048394cfe47d23142e196
HotLava03 <44071180+HotLava03@users.noreply.github.com>
2019-12-08 21:40:55 +0000

    Delete Category.java

--- src/main/java/io/github/hotlava03/baclava4j/commands/Category.java
+++
@@ -1,8 +0,0 @@
-package io.github.hotlava03.baclava4j.commands;
-
-import java.util.List;
-
-public interface Category {
-    public CategoryType getType();
-    public List<Command> getCommands();
-}
